2b08ed08cf352ef98f0d973803d69b595c336e34
jerry <845460348@qq.com>
2014-09-02 17:36:08 +0800

    y

---
+++ 2B9商城/js/js.js
@@ -0,0 +1,15 @@
+function chaxun(){
+		var s = document.getElementById('cx');
+	}
+
+
+function sy(){
+		var s = document.getElementById('l1');
+		alert("成功设为首页");
+	}
+	
+	
+function sc(){
+		var s = document.getElementById('l1');
+		alert("成功收藏本页");
+	}
